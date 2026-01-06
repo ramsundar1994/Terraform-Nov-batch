829d09a14b918cd338fffb9ad288230b228dc552
Ram <ram.sundar14194@gmail.com>
2026-01-06 11:20:44 +0530

    day8 spn added

--- Day-8-SPN-Creation/main.tf
+++ Day-8-SPN-Creation/main.tf
@@ -6,12 +6,19 @@ data "azurerm_subscription" "subscription" {
 resource "azuread_application" "app" {
   display_name = "terraform-spn-test"
 }
+# app secrets
+resource "azuread_application_password" "app-secret" {
+  display_name = "app-secret"
+  application_id = azuread_application.app.id
+}
 #service principal
 resource "azuread_service_principal" "spn" {
   client_id = azuread_application.app.client_id
 }
+
 # create password for service principal (SPN Secrets)
 resource "azuread_service_principal_password" "spn-psd" {
+  display_name = "spn-secret"
   service_principal_id = azuread_service_principal.spn.id
   end_date             = "2026-01-15T15:04:05Z" #1year
 }
